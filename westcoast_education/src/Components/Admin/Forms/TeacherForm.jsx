f9a1c9d95ab4d68aa3b2e822cc28f75c1752a72e
Tova Jertfelt <tovizitan@gmail.com>
2023-01-13 11:52:18 +0100

    checkbox går ej att trycka på i nuläget, kolla upp samt kolla upp post axios. använda strapi databas istället?

--- westcoast_education/src/Components/Admin/Forms/TeacherForm.jsx
+++ westcoast_education/src/Components/Admin/Forms/TeacherForm.jsx
@@ -1,18 +1,44 @@
 import styled from "styled-components";
 import CompetenceList from "./CompetenceCheckboxes";
-import { useRef } from "react";
-import { clear } from "@testing-library/user-event/dist/clear";
+import { useRef, useEffect, useCallback, useState } from "react";
+import axios from "axios";
+import Modal from "../../ui/Modal/Modal";
 
 const Form = styled.form``
+const Button = styled.input``
 
+const CheckBox = ({competence, isChecked, onAddCompetence, onRemoveCompetence}) => (
+  <div key = {competence.competenceID}>
+  <input 
+        type="checkbox"
+        id={`kompetens ${competence.competenceID}`}
+        checked={isChecked}
+        onChange={isChecked ? onAddCompetence : onRemoveCompetence}
+        name={`kompetens ${competence.competenceID}`}
+        value={competence.competence}/>
+        <label 
+        htmlFor={`kompetens ${competence.competenceID}`}>
+          {competence.competence}
+        </label>
+  </div>
+)
 
 const TeacherForm = () => {
+  const [competences, setCompetences] = useState([])
+  const [error, setError] = useState(null)
+  const [showModal, setShowModal] = useState(false)
+  const [modifiedData, setModifiedData] = useState({competences: [], description: '', name: ''})
   const firstNameInputRef = useRef()
   const lastNameInputRef = useRef()
   const personalIDInputRef = useRef()
   const emailInputRef = useRef()
   const mobileNoInputRef = useRef()
-  const competencelistRef = useRef()
+  const competenceURL = "http://localhost:8000/competences"
+  const teacherURL = "http://localhost:8000/teachers"
+
+  // const handleChangeCheckbox = useCallback(({target: {name, value}}) => {
+  //   setModifiedData(prevData => ({...prevData, [name]: value}))
+  // }, [])
 
   const clearForm = () => {
     firstNameInputRef.current.value = ""
@@ -20,17 +46,18 @@ const TeacherForm = () => {
     personalIDInputRef.current.value = ""
     emailInputRef.current.value =""
     mobileNoInputRef.current.value =""
-    competencelistRef.current.value = ""
+   
   }
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
+    
     const firstName = firstNameInputRef.current.value;
     const lastName = lastNameInputRef.current.value;
     const personalID = personalIDInputRef.current.value;
     const email = emailInputRef.current.value;
     const mobileNo = mobileNoInputRef.current.value;
-    const competences =  competencelistRef.current.value; 
+    
 
     const teacher = {
       firstName,
@@ -38,26 +65,49 @@ const TeacherForm = () => {
       personalID,
       email,
       mobileNo,
-      competences
+      modifiedData
     }
+    
+    await axios 
+    .post(teacherURL, teacher)
+    .then(res => {
+      console.log(res, "response from axios post")
+    })
+    .catch(error => {
+      setError(error)
+    })
     clearForm()
   }
 
+ 
+   if(error){
+    setShowModal(true)
+   }
+
+   useEffect(() => {
+    fetch(competenceURL)
+    .then(res => res.json())
+    .then(data => setCompetences(data))
+   }, [])
+
   return ( 
     <Form onSubmit={onSubmit}>
-      <label htmlFor="firstName">Förnamn/Tilltalsnamn:</label>
+      <label htmlFor="firstName">
+        Förnamn/Tilltalsnamn:</label>
       <input id="firstName"
       ref={firstNameInputRef}
       name="firstName"
       placeholder="Arnold"
       type="text"/>
-      <label htmlFor="lastName">Efternamn:</label>
+      <label htmlFor="lastName">
+        Efternamn:</label>
       <input id="lastName" 
       ref={lastNameInputRef}
       name="lastName"
       placeholder="Jertfelt"
       type="text"/>
-      <label htmlFor="personalID">Personnummer:</label>
+      <label htmlFor="personalID">
+        Personnummer:</label>
       <input id="personalID"
       placeholder="0000000"
       ref={personalIDInputRef}
@@ -65,22 +115,47 @@ const TeacherForm = () => {
       type="number"
       min="10"
       max="12"/>
-      <label htmlFor="email">Email:</label>
+      <label htmlFor="email">
+        Email:</label>
       <input id="email"
       ref={emailInputRef}
       placeholder="admin@westcoast.com"
       name="email"
       type="email"/>
-      <label htmlFor="mobileNo">Mobil:</label>
+      <label htmlFor="mobileNo">
+        Mobil:</label>
       <input id="mobileNo"
       ref={mobileNoInputRef}
       name="mobileNo"
       type="tel"
       pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
       placeholder="070000000"/>
-      <CompetenceList 
-      ref={competencelistRef}/>
-      <input type="submit"></input>
+      <div>
+      <p>Välj en eller flera kompetenser:</p>
+      {competences.map(competence => (
+        <CheckBox
+        competence={competence}
+              isChecked={modifiedData.competences.includes(competence.competenceID)}
+              onAddCategory={() =>
+                setModifiedData((prevData) => ({
+                  ...prevData,
+                  competences: [...prevData.competences, competence.competenceID],
+                }))
+              }
+              onRemoveCategory={() =>
+                setModifiedData((prevData) => ({
+                  ...prevData,
+                  categories: prevData.competences.filter(
+                    (id) => id !== competence.competenceID
+                  ),
+                }))
+              }
+            />
+      ))}
+      </div>
+      <Button
+      type="submit" 
+      value="Lägg till lärare"/>
     </Form>
     );
 }
