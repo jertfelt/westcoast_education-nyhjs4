c59b48df1c794d1612859488345cb6101b26e5a7
Tova Jertfelt <tovizitan@gmail.com>
2023-01-22 21:00:16 +0100

    fixat env process

--- src/firebase/initFirebase.js
+++ src/firebase/initFirebase.js
@@ -5,13 +5,14 @@ import { getDatabase } from 'firebase/database'
 
 
 const clientCredentials = {
-  apiKey: "AIzaSyBj_ocbD0O1j13HCB3Bv2LnDzflVzhlrVs",
-  authDomain: "weducation-7e828.firebaseapp.com",
-  databaseURL: "https://weducation-7e828-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "weducation-7e828",
-  storageBucket: "weducation-7e828.appspot.com",
-  messagingSenderId: "206585641573",
-  appId: "1:206585641573:web:fd2cd3322d30904b0ce48c"
+
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH, 
+  databaseURL: process.env.REACT_APP_DBURL, 
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MSGSENDERID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
 const initFirebase = () => {
