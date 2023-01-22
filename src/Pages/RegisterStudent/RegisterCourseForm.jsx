0881f00a2f616e572b37d26fcc3b692287183845
Tova Jertfelt <tovizitan@gmail.com>
2023-01-22 22:15:14 +0100

    halvvägs

--- src/Pages/RegisterStudent/RegisterCourseForm.jsx
+++ src/Pages/RegisterStudent/RegisterCourseForm.jsx
@@ -1,12 +1,14 @@
 import { useState, useRef, useContext, useEffect} from "react";
 import StudentContext from "../../Context/StudentContext";
 import { FormInstructions } from "../../Components/StylingElements/Form/Form";
-import Button from "../../Components/StylingElements/Buttons/FormButton";
 import {IfAlreadyExists, StudentContainer, TwoColumns} from "../../Components/StylingElements/StudentSections/StudentSections";
 import { useFirebase } from "../../Components/utils/useFirebase";
+import { getDatabase, ref, set } from "firebase/database";
+import { useNavigate } from "react-router-dom";
 
 
 const RegisterCourseForm = ({studentid, item, coursesInDB}) => {
+  const navigate = useNavigate()
   const context = useContext(StudentContext);
   const [validInputs, setValidInputs] = useState(false)
   const emailInputRef = useRef()
@@ -14,51 +16,92 @@ const RegisterCourseForm = ({studentid, item, coursesInDB}) => {
   const courseInputRef2 = useRef()
   const [studentEmail, setStudentEmail] = useState("")
   const [studentPassword, setStudentPassword] = useState("")
-
   const {data, error, loading} = useFirebase("/courses")
   const [courses, setCourses] = useState([])
-  const [firstChoice, setFirst] = useState([])
-  const [secondChoice, setSecond] = useState([])
-  const [secondToDb, setSecondToDb] = useState("")
+  const [firstChoice, setFirst] = useState("")
+  const [newSecondChoice, setNewSecond] = useState("Ingen")
+  const [secondChoice, setSecond] = useState("")
+
 
 console.log("id:", studentid, "kurser:", coursesInDB, "allt", item)
 
+const sendEditToFb = (
+    studentName,
+    IDtoDB,
+    studentEmail,
+    studentPassword,
+    courses
+  ) => {
+  const db = getDatabase()
+  set(ref(db, "/students/" + IDtoDB ),{
+    studentName: studentName,
+    studentID: IDtoDB,
+    studentEmail: studentEmail,
+    studentPassword: studentPassword,
+    courses: courses
+  }).then(
+    navigate("/student")
+  )
+}
+
+
 const onSubmit = (e) => {
+  e.preventDefault()
 
- const courseInputObligatory = courseInputRef.current.value
- const IDtoDB = studentid
-  const studentName = context.studentName
-
- if (courseInputRef2.current.value  === "Välj" || courseInputRef2.current.value  === "Ingen"){
-  if(secondChoice === "Välj" || secondChoice === "Ingen"){
-    setSecondToDb(false)
+const courseInputObligatory = courseInputRef.current.value
+let optional = ""
+const IDtoDB = studentid
+const studentName = context.studentName
+  if(newSecondChoice === "Välj" || newSecondChoice === "Ingen"){
+    optional = ""
   }
   else{
-    setSecondToDb(true)
+   optional = courseInputRef2.current.value
   }
+let courses = {
+  firstChoice : courseInputObligatory,
+  secondChoice : optional
 }
- else {
-  console.log(firstChoice, secondChoice)
- }
+
+  sendEditToFb(
+    studentName,
+    IDtoDB,
+    studentEmail,
+    studentPassword,
+    courses
+  )
+  
+}
 
 
-}
 
 const checkInputsFirstChoice = (e) => {
   e.preventDefault()
   if(e.target.value !== "Välj:"){
-    setFirst(e.target.value)
-    setValidInputs(true)
-  }else{
+    if(e.target.value !== courseInputRef2.current.value){
+      setValidInputs(true)
+    }
+    else{
+      setNewSecond("Ingen")
+    }
+    
+  }
+  else{
     setValidInputs(false)
   }
 }
 
 const checkInputsSecondChoice = (e) => {
   e.preventDefault()
-  if(e.target.value !== "Välj" || e.target.value !== "Ingen"){
-    setSecond(e.target.value)
+  if(e.target.value !== courseInputRef.current.value){
+    if(e.target.value !== "Välj" || e.target.value !== "Ingen"){
+      setNewSecond(courseInputRef2.current.value)
+    }
   }
+  else{
+    setNewSecond("Ingen")
+  }
+  
 }
 
 useEffect(() => {
@@ -71,8 +114,14 @@ useEffect(() => {
     setFirst(courses.map(item => item.firstChoice)[0])
     setSecond(courses.map(item => item.secondChoice)[0])
   }
-}, [item, coursesInDB, courses])
+}, [item,firstChoice, coursesInDB, courses])
 
+// const newPrio =(string, e) =>{
+//   e.preventDefault()
+//   if(string === "1"){
+    
+//   }
+// }
 
   return ( 
   <StudentContainer>
@@ -88,6 +137,7 @@ useEffect(() => {
     <h2>Du är anmäld till :</h2>
     <TwoColumns
     largergap>
+      
     <div>
     <h3>#1: {firstChoice}</h3>
     {data && data.filter(function (i){
@@ -96,7 +146,9 @@ useEffect(() => {
                 <p>Start: {item.startDate}</p>
                 <p>Längd i veckor: {item.lengthWeeks} </p>
                 <p>Lärare: {item.teacherAssigned}</p>
-                </div>))}
+                {/* <button onClick={(e) => newPrio("2",e)}>Byt till prio #2</button> */}
+                </div>
+                ))}
     </div>
     {secondChoice && <>
     <div>
@@ -108,10 +160,14 @@ useEffect(() => {
                 <p>Start: {item.startDate}</p>
                 <p>Längd i veckor: {item.lengthWeeks} </p>
                 <p>Lärare: {item.teacherAssigned}</p>
+                {/* <button onClick={(e) => newPrio("1",e)}>Byt till prio #1</button> */}
                 </div>))}
+                
     </div>
     </>}
+    
     </TwoColumns>
+ 
     </IfAlreadyExists>
     </>}
     <TwoColumns
@@ -131,9 +187,7 @@ useEffect(() => {
         data-testid="optionDefault"
         label={"Välj:"}/>
 
-        {data && data.filter(function (course){
-          return course.courseName !== firstChoice})
-          .filter(function (i){
+        {data && data.filter(function (i){
             return i.courseName !== "DELETED"
           }).map(item => ( 
         <option 
@@ -149,6 +203,7 @@ useEffect(() => {
       <label htmlFor="secondChoice">
         **Kursval 2:</label>
       <select id="secondChoice" 
+      ref={courseInputRef2}
       data-testid="studentcourse2"
       onChange={(e) => checkInputsSecondChoice(e)}
       >
@@ -157,10 +212,7 @@ useEffect(() => {
       name="Välj:"
       label="Välj:"/>
         {data && data.filter(function (i){
-            return i.courseName !== "DELETED"})
-            .filter(function (course){return course.courseName !== coursesInDB.firstChoice})
-            .filter(function (course){return course.courseName !== coursesInDB.secondChoice})
-            .map(item => ( <option key={item.courseID} value={item.courseName}>{item.courseName}</option>)
+            return i.courseName !== "DELETED"}).map(item => ( <option key={item.courseID} value={item.courseName}>{item.courseName}</option>)
         )}
         <option 
         value="Ingen"
